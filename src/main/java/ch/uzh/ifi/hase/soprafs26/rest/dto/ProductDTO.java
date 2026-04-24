c4c4b9058f1e4bd83d8f29a0e2e69ff87317aa0c
Yifu Li <liyifu0087@gmail.com>
2026-04-24 20:24:53 +0200

    Local dataset fallback if OFF API fail

--- src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/ProductDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/ProductDTO.java
@@ -12,6 +12,9 @@ public class ProductDTO {
   private String imageUrl;
   private String productUrl;
   private String nutriScore;
+  private Boolean localFallback;
+  private String dataSource;
+  private Double caloriesPerPackage;
 
   private List<String> stores;
   private List<String> storeTags;
@@ -21,115 +24,54 @@ public class ProductDTO {
   private Map<String, Object> nutriScoreData;
   private Map<String, Object> rawProduct;
 
-  public String getBarcode() {
-    return barcode;
-  }
+  public String getBarcode() { return barcode; }
+  public void setBarcode(String barcode) { this.barcode = barcode; }
 
-  public void setBarcode(String barcode) {
-    this.barcode = barcode;
-  }
+  public String getName() { return name; }
+  public void setName(String name) { this.name = name; }
 
-  public String getName() {
-    return name;
-  }
+  public String getBrand() { return brand; }
+  public void setBrand(String brand) { this.brand = brand; }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+  public String getQuantity() { return quantity; }
+  public void setQuantity(String quantity) { this.quantity = quantity; }
 
-  public String getBrand() {
-    return brand;
-  }
+  public String getServingSize() { return servingSize; }
+  public void setServingSize(String servingSize) { this.servingSize = servingSize; }
 
-  public void setBrand(String brand) {
-    this.brand = brand;
-  }
+  public String getImageUrl() { return imageUrl; }
+  public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
 
-  public String getQuantity() {
-    return quantity;
-  }
+  public String getProductUrl() { return productUrl; }
+  public void setProductUrl(String productUrl) { this.productUrl = productUrl; }
 
-  public void setQuantity(String quantity) {
-    this.quantity = quantity;
-  }
+  public String getNutriScore() { return nutriScore; }
+  public void setNutriScore(String nutriScore) { this.nutriScore = nutriScore; }
 
-  public String getServingSize() {
-    return servingSize;
-  }
+  public Boolean getLocalFallback() { return localFallback; }
+  public void setLocalFallback(Boolean localFallback) { this.localFallback = localFallback; }
 
-  public void setServingSize(String servingSize) {
-    this.servingSize = servingSize;
-  }
+  public String getDataSource() { return dataSource; }
+  public void setDataSource(String dataSource) { this.dataSource = dataSource; }
 
-  public String getImageUrl() {
-    return imageUrl;
-  }
+  public Double getCaloriesPerPackage() { return caloriesPerPackage; }
+  public void setCaloriesPerPackage(Double caloriesPerPackage) { this.caloriesPerPackage = caloriesPerPackage; }
 
-  public void setImageUrl(String imageUrl) {
-    this.imageUrl = imageUrl;
-  }
+  public List<String> getStores() { return stores; }
+  public void setStores(List<String> stores) { this.stores = stores; }
 
-  public String getProductUrl() {
-    return productUrl;
-  }
+  public List<String> getStoreTags() { return storeTags; }
+  public void setStoreTags(List<String> storeTags) { this.storeTags = storeTags; }
 
-  public void setProductUrl(String productUrl) {
-    this.productUrl = productUrl;
-  }
+  public List<String> getPurchasePlaces() { return purchasePlaces; }
+  public void setPurchasePlaces(List<String> purchasePlaces) { this.purchasePlaces = purchasePlaces; }
 
-  public String getNutriScore() {
-    return nutriScore;
-  }
+  public Map<String, Object> getNutriments() { return nutriments; }
+  public void setNutriments(Map<String, Object> nutriments) { this.nutriments = nutriments; }
 
-  public void setNutriScore(String nutriScore) {
-    this.nutriScore = nutriScore;
-  }
+  public Map<String, Object> getNutriScoreData() { return nutriScoreData; }
+  public void setNutriScoreData(Map<String, Object> nutriScoreData) { this.nutriScoreData = nutriScoreData; }
 
-  public List<String> getStores() {
-    return stores;
-  }
-
-  public void setStores(List<String> stores) {
-    this.stores = stores;
-  }
-
-  public List<String> getStoreTags() {
-    return storeTags;
-  }
-
-  public void setStoreTags(List<String> storeTags) {
-    this.storeTags = storeTags;
-  }
-
-  public List<String> getPurchasePlaces() {
-    return purchasePlaces;
-  }
-
-  public void setPurchasePlaces(List<String> purchasePlaces) {
-    this.purchasePlaces = purchasePlaces;
-  }
-
-  public Map<String, Object> getNutriments() {
-    return nutriments;
-  }
-
-  public void setNutriments(Map<String, Object> nutriments) {
-    this.nutriments = nutriments;
-  }
-
-  public Map<String, Object> getNutriScoreData() {
-    return nutriScoreData;
-  }
-
-  public void setNutriScoreData(Map<String, Object> nutriScoreData) {
-    this.nutriScoreData = nutriScoreData;
-  }
-
-  public Map<String, Object> getRawProduct() {
-    return rawProduct;
-  }
-
-  public void setRawProduct(Map<String, Object> rawProduct) {
-    this.rawProduct = rawProduct;
-  }
+  public Map<String, Object> getRawProduct() { return rawProduct; }
+  public void setRawProduct(Map<String, Object> rawProduct) { this.rawProduct = rawProduct; }
 }
