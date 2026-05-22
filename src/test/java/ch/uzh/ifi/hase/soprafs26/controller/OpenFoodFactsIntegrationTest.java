de5a9c8a0401f1ce328a007d3f35baffcaf75cd6
u826318 <c05811468@gmail.com>
2026-05-22 04:53:20 +0200

    test: update OpenFoodFactsIntegrationTest to mock OFF service and reflect fallback flow

--- src/test/java/ch/uzh/ifi/hase/soprafs26/controller/OpenFoodFactsIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs26/controller/OpenFoodFactsIntegrationTest.java
@@ -16,14 +16,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.server.ResponseStatusException;
 
 import ch.uzh.ifi.hase.soprafs26.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs26.entity.User;
 import ch.uzh.ifi.hase.soprafs26.repository.UserRepository;
 import ch.uzh.ifi.hase.soprafs26.rest.dto.localdataset.LocalDatasetProductDTO;
+import ch.uzh.ifi.hase.soprafs26.service.OpenFoodFactsService;
 import ch.uzh.ifi.hase.soprafs26.service.localdatasetlookup.LocalDatasetLookupService;
 import ch.uzh.ifi.hase.soprafs26.service.localdatasetlookup.LocalDatasetProductMapper;
 
@@ -40,6 +43,9 @@ class OpenFoodFactsIntegrationTest {
     private UserRepository userRepository;
 
     @MockitoBean
+    private OpenFoodFactsService openFoodFactsService;
+
+    @MockitoBean
     private LocalDatasetLookupService localDatasetLookupService;
 
     @MockitoBean
@@ -52,7 +58,10 @@ class OpenFoodFactsIntegrationTest {
     }
 
     @Test
-    void lookupByBarcode_authenticatedRequest_returnsMappedProductFromLocalDataset() throws Exception {
+    void lookupByBarcode_offFails_returnsMappedProductFromLocalDataset() throws Exception {
+        given(openFoodFactsService.lookupByBarcode("7610848492087"))
+                .willThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "not found in OFF"));
+
         LocalDatasetProductDTO dto = new LocalDatasetProductDTO();
         dto.setBarcode("7610848492087");
         dto.setName("Integration Test Chocolate");
@@ -90,7 +99,9 @@ class OpenFoodFactsIntegrationTest {
     }
 
     @Test
-    void lookupByBarcode_barcodeNotInLocalDataset_returns404() throws Exception {
+    void lookupByBarcode_notFoundInOffOrLocal_returns404() throws Exception {
+        given(openFoodFactsService.lookupByBarcode("0000"))
+                .willThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "not found in OFF"));
         given(localDatasetLookupService.findRawRowByBarcode("0000")).willReturn(Optional.empty());
 
         mockMvc.perform(get("/products/lookup")
